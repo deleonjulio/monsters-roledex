61908946253712eebff84df26e37d414ffa6ed1f
julio <julio@tenelleven.com>
2020-05-02 23:16:54 +0800

    Updates

---
+++ precache-manifest.aa8e631d2723cd3e5e39b74ef338b943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e728f47612f8360e38f06098e38d91",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "0eb96a8f59799eb2b853",
+    "url": "/monsters-roledex/static/css/main.97d074b0.chunk.css"
+  },
+  {
+    "revision": "c8a61f426f3390d4f665",
+    "url": "/monsters-roledex/static/js/2.8b700e42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.8b700e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb96a8f59799eb2b853",
+    "url": "/monsters-roledex/static/js/main.273821b7.chunk.js"
+  },
+  {
+    "revision": "72ebccc5c6e31a34e20e",
+    "url": "/monsters-roledex/static/js/runtime-main.3363bd7e.js"
+  }
+]);
